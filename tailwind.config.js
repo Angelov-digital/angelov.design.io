8237a35cdbd1d1d07c024cb597b535c0dc81de8b
Angelov-digital <96943583+Angelov-digital@users.noreply.github.com>
2024-09-05 17:53:31 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        aquire: ['aquire', 'sans-serif'], 
+       
+      },
+      colors: {
+        'blue': '#232534', 
+        'lavender': '#F0E9F9', 
+        'light': '#F8F3FF', 
+        'shade': '#2325348C', 
+        'dark' : '#17171e',
+        'light-trans' : '#f8f3ffef',
+      },
+    },
   },
   plugins: [],
 }
