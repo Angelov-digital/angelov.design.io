b12eff4d5726e1204bf7fcfddcbda73d3648141b
Angelov-digital <adamangelo.osvc@gmail.com>
2024-11-17 21:41:54 +0100

    links, styles, finishing

--- js/main.js
+++ js/main.js
@@ -100,45 +100,27 @@ win.scroll(function(event) {
   
 });
 
-
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+      modal.classList.add('show'); // Show the modal
+      // Add an event listener to close the modal when clicking outside the image
+      modal.addEventListener('click', (event) => {
+          if (event.target === modal) {
+              closeModal(modalId);
+          }
+      });
+  } else {
+      console.error(`Modal with ID "${modalId}" not found.`);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+      modal.classList.remove('show'); // Hide the modal
+  } else {
+      console.error(`Modal with ID "${modalId}" not found.`);
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
